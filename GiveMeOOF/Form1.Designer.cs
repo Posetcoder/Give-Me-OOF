d7c2de636654931712b0e48a4cb8dff6fb4c954a
Mehrad Soheili <admin@softwareprince.com>
2022-07-29 14:07:03 +1000

    first version

---
+++ GiveMeOOF/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GiveMeOOF
+{
+    partial class GiveMeOof
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GiveMeOof));
+            this.changeOof = new System.Windows.Forms.Button();
+            this.changeAllVersionsOof = new System.Windows.Forms.CheckBox();
+            this.versionTextBoxOof = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // changeOof
+            // 
+            this.changeOof.BackColor = System.Drawing.Color.Gray;
+            this.changeOof.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeOof.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.changeOof.Location = new System.Drawing.Point(12, 120);
+            this.changeOof.Name = "changeOof";
+            this.changeOof.Size = new System.Drawing.Size(193, 37);
+            this.changeOof.TabIndex = 0;
+            this.changeOof.Text = "Change Sound To OOF";
+            this.changeOof.UseVisualStyleBackColor = false;
+            this.changeOof.Click += new System.EventHandler(this.changeOof_Click);
+            // 
+            // changeAllVersionsOof
+            // 
+            this.changeAllVersionsOof.AutoSize = true;
+            this.changeAllVersionsOof.Checked = true;
+            this.changeAllVersionsOof.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.changeAllVersionsOof.Location = new System.Drawing.Point(43, 25);
+            this.changeAllVersionsOof.Name = "changeAllVersionsOof";
+            this.changeAllVersionsOof.Size = new System.Drawing.Size(118, 17);
+            this.changeAllVersionsOof.TabIndex = 1;
+            this.changeAllVersionsOof.Text = "Change all versions";
+            this.changeAllVersionsOof.UseVisualStyleBackColor = true;
+            this.changeAllVersionsOof.CheckedChanged += new System.EventHandler(this.changeAllVersionsOof_CheckedChanged);
+            // 
+            // versionTextBoxOof
+            // 
+            this.versionTextBoxOof.Location = new System.Drawing.Point(52, 81);
+            this.versionTextBoxOof.Name = "versionTextBoxOof";
+            this.versionTextBoxOof.Size = new System.Drawing.Size(100, 20);
+            this.versionTextBoxOof.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter version:";
+            // 
+            // GiveMeOof
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 169);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.versionTextBoxOof);
+            this.Controls.Add(this.changeAllVersionsOof);
+            this.Controls.Add(this.changeOof);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GiveMeOof";
+            this.Text = "Give Me OOF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button changeOof;
+        private System.Windows.Forms.CheckBox changeAllVersionsOof;
+        private System.Windows.Forms.TextBox versionTextBoxOof;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GiveMeOOF/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GiveMeOOF
+{
+    public partial class GiveMeOof : Form
+    {
+
+        string rbxPath = @"C:\Users\" + Environment.UserName + @"\AppData\Local\Roblox\Versions";
+
+        public GiveMeOof()
+        {
+            InitializeComponent();
+            changeAllVersionsOof.Checked = true;
+            versionTextBoxOof.Text = "";
+            versionTextBoxOof.Enabled = false;
+            label1.Enabled = false;
+        }
+
+        private void changeOof_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(rbxPath))
+            {
+                if (changeAllVersionsOof.Checked)
+                {
+
+                    foreach (string directory in Directory.GetDirectories(rbxPath))
+                    {
+                        string dirstr = directory;
+                        string dirname = dirstr.Substring(dirstr.LastIndexOf(@"\") + 1);
+
+                        Console.WriteLine("dirstr: " + dirstr);
+                        Console.WriteLine("dirname: " + dirname);
+
+                        DirectoryInfo dir = new DirectoryInfo(dirstr);
+
+                        if (dir.Exists)
+                        {
+                            if (dirname.Contains("version"))
+                            {
+                                string soundPath = dirstr + "\\content\\sounds\\";
+
+
+                                try
+                                {
+                                    Console.WriteLine("soundPath: " + soundPath);
+                                    Extract("GiveMeOOF", soundPath, "Resources", "oof.ogg");
+                                    if (File.Exists(soundPath + "ouch.ogg")) File.Delete(soundPath + "ouch.ogg");
+                                    File.Move(soundPath + "oof.ogg", soundPath + "ouch.ogg");
+
+                                }
+                                catch { MessageBox.Show("Sorry, an unexpected error occured. Please try again", "Give Me OOF", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    string version = versionTextBoxOof.Text;
+                    string versionPath = rbxPath + "\\" + version;
+                    if (Directory.Exists(versionPath))
+                    {
+                        string dirstr = versionPath;
+                        string dirname = version;
+
+                        Console.WriteLine("dirstr: " + dirstr);
+                        Console.WriteLine("dirname: " + dirname);
+
+                        DirectoryInfo dir = new DirectoryInfo(dirstr);
+
+                        if (dir.Exists)
+                        {
+                            if (dirname.Contains("version"))
+                            {
+                                string soundPath = dirstr + "\\content\\sounds\\";
+
+
+                                try
+                                {
+                                    Console.WriteLine("soundPath: " + soundPath);
+                                    Extract("GiveMeOOF", soundPath, "Resources", "oof.ogg");
+                                    if (File.Exists(soundPath + "ouch.ogg")) File.Delete(soundPath + "ouch.ogg");
+                                    File.Move(soundPath + "oof.ogg", soundPath + "ouch.ogg");
+
+                                }
+                                catch { MessageBox.Show("Sorry, an unexpected error occured. Please try again", "Give Me OOF", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("That version does not exist in your Roblox folder. Please enter a valid version and try again.", "Give Me OOF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("That version does not exist in your Roblox folder. Please enter a valid version and try again.", "Give Me OOF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("You dont have Roblox installed! Please install Roblox and try again.", "Give Me OOF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void changeAllVersionsOof_CheckedChanged(object sender, EventArgs e)
+        {
+            if (changeAllVersionsOof.Checked)
+            {
+                versionTextBoxOof.Text = "";
+                versionTextBoxOof.Enabled = false;
+                label1.Enabled = false;
+            }
+            else
+            {
+                versionTextBoxOof.Enabled = true;
+                label1.Enabled = true;
+            }
+        }
+
+        private static void Extract(string nameSpace, string outputDir, string resourceLocation, string file)
+        {
+            using (System.IO.Stream stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(nameSpace + "." + resourceLocation + @"." + file))
+            {
+                using (System.IO.FileStream fileStream = new System.IO.FileStream(System.IO.Path.Combine(outputDir, file), System.IO.FileMode.Create))
+                {
+                    for (int i = 0; i < stream.Length; i++)
+                    {
+                        fileStream.WriteByte((byte)stream.ReadByte());
+                    }
+                    fileStream.Close();
+                }
+            }
+        }
+    }
+}
